d58bc904ba780b72d6ad7794046f363f5fd6fec8
henilhp6641 <henil.pavasiya@gmail.com>
2024-03-06 17:33:14 -0500

    Main

--- app/src/main/java/com/example/firebasepushnotifications/MainActivity.kt
+++ app/src/main/java/com/example/firebasepushnotifications/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.firebasepushnotifications
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -12,34 +13,14 @@ import com.example.firebasepushnotifications.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: TestViewModel
     lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setTheme(R.style.Theme_FirebasePushNotifications)
         setContentView(binding.root)
 
-        viewModel = ViewModelProvider(this)[TestViewModel::class.java]
+        Log.d("Log","Hello, how are you?")
 
-        viewModel.currentNumber.observe(this, Observer {
-            binding.tvCurrentNumber.text = it.toString()
-        })
 
-        viewModel.currentBoolean.observe(this, Observer {
-            binding.tvCurrentBoolean.text = it.toString()
-        })
-
-        incrementText()
-    }
-
-    private fun incrementText() {
-        binding.btn.setOnClickListener {
-            viewModel.currentNumber.value = viewModel.number++
-            viewModel.currentBoolean.value = viewModel.number % 2 == 0
-//            binding.tvCurrentNumber.text =  viewModel.currentNumber.value.toString() //Using this method, we can also save data into UI without using Observer
-//            binding.tvCurrentBoolean.text =  viewModel.currentBoolean.value.toString()
-        }
     }
 }
